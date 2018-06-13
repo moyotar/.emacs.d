e2629ca39407993e1cffa70b6f7b1b11584adc92
moyotar <moyotar.60e@gmail.com>
2018-06-13 17:43:18 +0800

    Add Explict packages which should be installed by el-get

--- config/init-el-get.el
+++ config/init-el-get.el
@@ -7,7 +7,11 @@
   (package-install 'el-get)
   (require 'el-get))
 
-(el-get 'sync)
+;; Note: Need to update my-packages's value after installed a new package every time.
+(setq my-packages
+      '(ace-jump-mode ace-mc ample-regexps anaphora auto-highlight-symbol avy bash-completion cl-lib color-theme-zenburn company-lua company-mode dash deferred el-get emacs-async emacs-ycmd epl expand-region f flycheck fold-this ghub helm helm-ag helm-gtags helm-projectile helm-smex json-mode json-reformat json-snatcher let-alist lua-mode magit magit-popup markdown-mode multiple-cursors nav-flash org-bullets package paredit pkg-info popup projectile rainbow-delimiters request rich-minority s seq smart-mode-line smartparens smex vlfi with-editor yasnippet yasnippet-snippets))
+
+(el-get 'sync my-packages)
 
 (custom-set-variables
  '(safe-local-variable-values
