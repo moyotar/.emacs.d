84315dcc6177f0f66f185a7c29c6b092f7317373
moyotar <moyotar.60e@gmail.com>
2018-08-08 11:53:31 +0800

    override zenburn-orange value

--- config/init-theme.el
+++ config/init-theme.el
@@ -1,5 +1,5 @@
 
-(defvar zenburn-override-colors-alist
+(setq zenburn-override-colors-alist
   '(("zenburn-orange" . "#CD853F")))
 
 (custom-set-variables
@@ -8,7 +8,6 @@
     ("65a2801c39211a69af6cc4c8441f3f51871404b9d12a6f2966a7bc33468995c7" default))))
 
 (load-theme 'zenburn t)
-
 
 
 (provide 'init-theme)
