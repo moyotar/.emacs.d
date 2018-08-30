2d76716dac113df340a8b6e5a389c48a84d08807
moyotar <moyotar.60e@gmail.com>
2018-08-30 15:48:09 +0800

    Disable flycheck-highlighting-mode at all

--- config/init-flycheck.el
+++ config/init-flycheck.el
@@ -1,4 +1,9 @@
 (require 'flycheck)
+
+;; Do not highlight anything at all.
+;; See check results by left fringe
+(setq flycheck-highlighting-mode nil)
+
 (add-hook 'prog-mode-hook #'global-flycheck-mode)
 
 (provide 'init-flycheck)
