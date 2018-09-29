8e42dd5486c3757ab1003ef2c6b18da1a0b97aa6
moyotar <moyotar.60e@gmail.com>
2018-09-29 10:21:26 +0800

    bind global key <f5> to compile command

--- config/init-basic.el
+++ config/init-basic.el
@@ -105,4 +105,9 @@
 
 (setq source-directory (expand-file-name "emacs-source" user-emacs-directory))
 
+(global-set-key (kbd "<f5>") (lambda ()
+			       (interactive)
+			       (setq-local compilation-read-command nil)
+			       (call-interactively 'compile)))
+
 (provide 'init-basic)
