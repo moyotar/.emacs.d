e73394ea58746aa2e1715084a01d517c832f5a93
moyotar <moyotar.60e@gmail.com>
2018-10-23 17:08:27 +0800

    add g17 word config

---
+++ config/init-g17-work.el
@@ -0,0 +1,26 @@
+;; g17 work config
+
+(add-to-list 'auto-mode-alist '("\\.pto$" . lua-mode))
+
+(defun g17-exec-cmd-1(cmd)
+  (let* ((root (projectile-project-root))
+	 (conf-file-path (expand-file-name "engine/etc/g17.conf" root))
+	 (port nil))
+    (if (file-exists-p conf-file-path)
+	(progn (setq port
+		      (shell-command-to-string
+		       (format "grep '^server_id' %s | awk '{gsub(\"[^0-9]+\", \"\"); print $0}' | tr -d '\n' " conf-file-path)))
+	       (message (shell-command-to-string (format "echo '%s' | nc -q 1 127.0.0.1 %s0" cmd port)))
+	       )))
+  )
+
+(defun g17-exec-cmd(cmd)
+  (interactive "M")
+  (g17-exec-cmd-1 cmd))
+
+(defun g17-update()
+  (interactive)
+  (g17-exec-cmd (format "ls Update(\"%s\")" (file-relative-name (buffer-file-name) (format "%s/logic" (projectile-project-root))))))
+
+(provide 'init-g17-work)
+
