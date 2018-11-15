58a73b808a125cdcb6ccaa9e9cd440b5dc640671
moyotar <moyotar.60e@gmail.com>
2018-11-15 10:04:40 +0800

    Update my-packages's value

--- config/init-el-get.el
+++ config/init-el-get.el
@@ -8,8 +8,9 @@
         (eval-print-last-sexp)))
 
 ;; Note: Need to update my-packages's value after installed a new package every time.
+;;  `(setq my-packages ',(mapcar #'el-get-as-symbol (el-get-list-package-names-with-status "installed")))
 (setq my-packages
-      '(ace-jump-mode ace-mc ample-regexps anaphora auto-highlight-symbol avy cl-lib color-theme-zenburn company-lua company-mode dash deferred el-get emacs-async emacs-ycmd epl expand-region f flycheck fold-this ghub helm helm-ag helm-gtags helm-projectile helm-smex json-mode json-reformat json-snatcher let-alist lua-mode magit magit-popup markdown-mode multiple-cursors nav-flash org-bullets package paredit pkg-info popup projectile rainbow-delimiters request rich-minority s seq smart-mode-line smartparens smex vlfi with-editor yasnippet yasnippet-snippets))
+      '(ace-jump-mode ace-mc ample-regexps anaphora auto-highlight-symbol avy bash-completion cl-lib color-theme-zenburn company-lua company-mode dash deferred el-get emacs-async emacs-ycmd epl expand-region f flycheck fold-this ghub graphql helm helm-ag helm-gtags helm-projectile helm-smex json-mode json-reformat json-snatcher let-alist lua-mode magit magit-popup markdown-mode multiple-cursors nav-flash org-bullets package paredit pkg-info popup projectile rainbow-delimiters request rich-minority s seq smart-mode-line smartparens smex treepy vlfi with-editor yasnippet yasnippet-snippets))
 
 (when (equal system-type 'gnu/linux)
   (setq my-packages (append my-packages '(bash-completion))))
