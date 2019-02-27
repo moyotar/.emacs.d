63904d79c9a17bf1f3e0512f09b379fc30f281f9
moyotar <moyotar.60e@gmail.com>
2019-02-27 16:10:21 +0800

    add org config

--- config/init-org.el
+++ config/init-org.el
@@ -1,5 +1,22 @@
+(require 'org)
+(require 'ox-md)
+
 (setq org-src-fontify-natively t)
 
 (add-hook 'org-mode-hook (lambda () (org-bullets-mode 1)))
+
+(define-key org-mode-map (kbd "C-c ;") nil)
+(define-key org-mode-map (kbd "C-c .") nil)
+(define-key org-mode-map (kbd "C-c t") 'org-time-stamp)
+
+(org-babel-do-load-languages
+      'org-babel-load-languages
+      '((emacs-lisp . t)
+        (lua . t)
+	(python . t)
+	(shell . t)
+	(dot . t)))
+
+(setq org-confirm-babel-evaluate nil)
 
 (provide 'init-org)
--- config/init-smartparens.el
+++ config/init-smartparens.el
@@ -2,6 +2,9 @@
 (require 'smartparens-config)
 (eval-after-load 'smartparens-config '(progn
 					(sp-use-paredit-bindings)
+					(dolist (key '("M-<up>" "M-<down>" "C-<right>"
+						       "C-<left>" "C-M-<left>" "C-M-<right>"))
+					  (define-key smartparens-mode-map (kbd key) nil))
 					(message "Finished use paredit key bindings.")))
 
 (smartparens-global-mode t)
