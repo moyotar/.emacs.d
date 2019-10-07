666d401dd19ab7966aa1d0411465b5f5599a897a
moyotar <792136980@qq.com>
2019-10-07 10:41:09 +0800

    add bash-completion only in gnu/linux

--- config/init-el-get.el
+++ config/init-el-get.el
@@ -10,7 +10,7 @@
 ;; Note: Need to update my-packages's value after installed a new package every time.
 ;;  `(setq my-packages ',(mapcar #'el-get-as-symbol (el-get-list-package-names-with-status "installed")))
 (setq my-packages
-      '(ace-jump-mode ace-mc ace-window ample-regexps anaphora auto-highlight-symbol avy bash-completion cl-lib color-theme-zenburn company-lua company-mode dash deferred el-get emacs-async epl expand-region f flycheck fold-this ghub graphql helm helm-ag helm-gtags helm-projectile helm-smex htmlize json-mode json-reformat json-snatcher let-alist lua-mode magit magit-popup markdown-mode multiple-cursors nav-flash org-bullets package paredit pkg-info popup projectile rainbow-delimiters request rich-minority s seq smart-mode-line smartparens smex treepy vlfi with-editor yasnippet yasnippet-snippets))
+      '(ace-jump-mode ace-mc ace-window ample-regexps anaphora auto-highlight-symbol avy cl-lib color-theme-zenburn company-lua company-mode dash deferred el-get emacs-async epl expand-region f flycheck fold-this ghub graphql helm helm-ag helm-gtags helm-projectile helm-smex htmlize json-mode json-reformat json-snatcher let-alist lua-mode magit magit-popup markdown-mode multiple-cursors nav-flash org-bullets package paredit pkg-info popup projectile rainbow-delimiters request rich-minority s seq smart-mode-line smartparens smex treepy vlfi with-editor yasnippet yasnippet-snippets))
 
 (when (equal system-type 'gnu/linux)
   (setq my-packages (append my-packages '(bash-completion))))
