2ed1cab441b1765ebb0a6040ce3b97e55379d79b
moyotar <moyotar.60e@gmail.com>
2025-04-18 11:57:10 +0800

    config copilot-completion-map

--- init.el
+++ init.el
@@ -610,10 +610,13 @@
   :hook (prog-mode . copilot-mode)
   :config
   ;; 配置补全接受方式
+  (define-key copilot-completion-map (kbd "TAB") 'copilot-accept-completion)
   (define-key copilot-completion-map (kbd "<tab>") 'copilot-accept-completion)
   (define-key copilot-completion-map (kbd "C-M-f") 'copilot-accept-completion-by-word)
   (define-key copilot-completion-map (kbd "C-M-l") 'copilot-accept-completion-by-line)
-  
+  (define-key copilot-completion-map (kbd "C-M-n") 'copilot-next-completion)
+  (define-key copilot-completion-map (kbd "C-M-p") 'copilot-previous-completion)
+
   ;; 其他自定义设置
   (setq copilot-max-char -1)
   ;; (setq copilot-enable-predicates '(copilot--buffer-changed)
